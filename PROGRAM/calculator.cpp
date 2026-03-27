708f34c7f1a35bb9cd12301807a55e21d60ce3f0
pisitpongth <pisitpong242550@gmail.com>
2026-03-27 18:31:34 +0700

    create PROGRAM folder and add calculator.cpp

---
+++ PROGRAM/calculator.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  char op;
+  double firstNumber;
+  double secondNumber;
+  double result;
+
+  cout << "********** CALCULATOR **********" << "\n";
+
+  cout << "Enter first number: ";
+  cin >> firstNumber;
+
+  cout << "Enter second number: ";
+  cin >> secondNumber;
+
+  cout << "Enter an operation: ";
+  cin >> op;
+
+  switch (op) {
+    case '+':
+      result = firstNumber + secondNumber;
+      cout << "The Result of " << firstNumber << " + " << secondNumber << " = "
+           << result << "\n";
+      break;
+    case '-':
+      result = firstNumber - secondNumber;
+      cout << "The Result of " << firstNumber << " - " << secondNumber << " = "
+           << result << "\n";
+      break;
+    case 'x':
+      result = firstNumber * secondNumber;
+      cout << "The Result of " << firstNumber << " x " << secondNumber << " = "
+           << result << "\n";
+      break;
+    case '/':
+      result = firstNumber / secondNumber;
+      cout << "The Result of " << firstNumber << " / " << secondNumber << " = "
+           << result << "\n";
+      break;
+    default:
+      cout << "That wasn't a valid operator" << "\n";
+      break;
+  }
+  cout << "********************************";
+
+  return 0;
+}
